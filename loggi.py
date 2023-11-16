367ccaa2945371bfd37da8060bc9e26971322424
jungkookswife <146013486+jungkookswife@users.noreply.github.com>
2023-11-16 18:12:59 +0500

    Проверка на правильность ввода

--- loggi.py
+++ loggi.py
@@ -3,3 +3,13 @@ import logging
 
 logging.basicConfig(filename='logi_file.log', level=logging.INFO)
 
+def get_user_input():
+    while True:
+        try:
+            n = int(input("Введите натуральное число: "))
+            if n <= 0:
+                print("Введите положительное число!")
+            else:
+                return n
+        except ValueError:
+            print("Неверный ввод. Пожалуйста, введите число.")
